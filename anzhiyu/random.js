b641d872a3b80e082fe767f599b1e67bad093b74
李则睿 <965584385@qq.com>
2025-01-11 03:36:31 +0800

    Site updated: 2025-01-11 03:36:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/archives/1711676290717"];function toRandomPost(){
+var posts=["2024/03/29/Vshell安装教程-C2平台的使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
